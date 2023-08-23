28755278758ae0cd7bb7a4af3e9cb6af6464a60f
KensieM <47368672+KensieM@users.noreply.github.com>
2023-08-23 15:38:20 -0600

    create page

--- src/components/create-nft/create-nft.tsx
+++ src/components/create-nft/create-nft.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react';
+import cn from 'classnames';
 import { NextSeo } from 'next-seo';
+import { Transition } from '@/components/ui/transition';
+import { RadioGroup } from '@/components/ui/radio-group';
+import { Listbox } from '@/components/ui/listbox';
+import Image from '@/components/ui/image';
+import Button from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
 import Input from '@/components/ui/forms/input';
 import Textarea from '@/components/ui/forms/textarea';
+import Uploader from '@/components/ui/forms/uploader';
 import InputLabel from '@/components/ui/input-label';
 import ToggleBar from '@/components/ui/toggle-bar';
 import { ChevronDown } from '@/components/icons/chevron-down';
+import { Ethereum } from '@/components/icons/ethereum';
+import { Flow } from '@/components/icons/flow';
+import { Warning } from '@/components/icons/warning';
 import { Unlocked } from '@/components/icons/unlocked';
-// images
+import Avatar from '@/components/ui/avatar';
+//images
+import AuthorImage from '@/assets/images/author.jpg';
 import NFT1 from '@/assets/images/nft/nft-1.jpg';
+import PriceType from '@/components/create-nft/price-types-props';
 
 const BlockchainOptions = [
   {
@@ -30,46 +44,163 @@ export default function CreateNFT() {
   let [unlocked, setUnlocked] = useState(false);
   let [priceType, setPriceType] = useState('fixed');
   let [blockchain, setBlockChain] = useState(BlockchainOptions[0]);
-  let [cpus, setCPUs] = useState('');
-  let [ram, setRAM] = useState('');
-  let [storage, setStorage] = useState('');
-  let [selectedRegion, setSelectedRegion] = useState('');
-
   return (
     <>
       <NextSeo
-        title="Launch"
+        title="Create NFT"
         description="GPU-Enabled Jupyter Notebook Deployment Platform"
       />
       <div className="mx-auto w-full sm:pt-0 lg:px-8 xl:px-10 2xl:px-0">
-        {/* ...Other parts of your code... */}
-        
-        {/* CPUs */}
-        <div className="mb-4">
-          <InputLabel title="CPUs" important />
-          <Input type="number" placeholder="Enter number of CPUs" onChange={(e) => setCPUs(e.target.value)} />
+        <h2 className="mb-6 text-lg font-medium uppercase tracking-wider text-gray-900 dark:text-white sm:mb-10 sm:text-2xl">
+          Create New Deployment
+        </h2>
+        <div className="mb-8 grid grid-cols-1 gap-12 lg:grid-cols-3">
+          <div className="lg:col-span-2">
+            {/* File uploader */}
+            <div className="mb-8">
+              <InputLabel title="Upload file" important />
+              <Uploader />
+            </div>
+
+            {/* NFT preview */}
+            <InputLabel title="Preview" />
+            <div className="relative flex flex-grow flex-col overflow-hidden rounded-lg bg-white shadow-card transition-all duration-200 hover:shadow-large dark:bg-light-dark">
+              <div className="flex items-center p-4 text-sm font-medium text-gray-600 transition hover:text-gray-900 dark:text-gray-400">
+                <Avatar
+                  size="sm"
+                  image={AuthorImage}
+                  alt="NateBolam"
+                  className="border-white bg-gray-300 ltr:mr-3 rtl:ml-3 dark:bg-gray-400"
+                />
+                @NateBolam
+              </div>
+              <div className="relative block w-full pb-full">
+                <Image
+                  src={NFT1}
+                  placeholder="blur"
+                  layout="fill"
+                  objectFit="cover"
+                  alt="Quantum Computing Simulations"
+                />
+              </div>
+              <div className="p-5">
+                <div className="text-sm font-medium text-black dark:text-white">
+                Quantum Computing Simulations
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
-        {/* RAM */}
-        <div className="mb-4">
-          <InputLabel title="RAM" important />
-          <Input type="number" placeholder="Enter RAM" onChange={(e) => setRAM(e.target.value)} />
+        {/* Price */}
+        <div className="mb-8">
+          <InputLabel title="Price Range" important />
+          <Input
+            min={0}
+            type="number"
+            placeholder="Enter your price range"
+            inputClassName="spin-button-hidden"
+          />
         </div>
 
-        {/* Storage */}
-        <div className="mb-4">
-          <InputLabel title="Storage" important />
-          <Input type="number" placeholder="Enter storage" onChange={(e) => setStorage(e.target.value)} />
+        {/* Name */}
+        <div className="mb-8">
+          <InputLabel title="Name" important />
+          <Input type="text" placeholder="Item name" />
         </div>
 
-        {/* Region */}
-        <div className="mb-4">
-          <InputLabel title="Region" important />
-          <Input type="text" placeholder="Enter region" onChange={(e) => setSelectedRegion(e.target.value)} />
+        {/* Description */}
+        <div className="mb-8">
+          <InputLabel
+            title="Description"
+            subTitle="The description will be included on the deployment's detail page."
+          />
+          <Textarea placeholder="Provide a detailed description of your deployment" />
         </div>
 
-        {/* ... Other fields ... */}
-        
+        <div className="mb-4">
+  <InputLabel title="CPUs" important />
+  <Input type="number" placeholder="Enter number of CPUs" onChange={(e) => setCPUs(e.target.value)} />
+</div>
+
+
+        {/* Unlockable content */}
+        <div className="mb-3">
+          <ToggleBar
+            title="Unlockable Content"
+            subTitle="Include unlockable content that can only be revealed by the owner of the item."
+            icon={<Unlocked />}
+            checked={unlocked}
+            onChange={() => setUnlocked(!unlocked)}
+          >
+            {unlocked && (
+              <Textarea placeholder="Enter content (access key, code to redeem, link to a file, etc.)" />
+            )}
+          </ToggleBar>
+        </div>
+
+        {/* Explicit content */}
+        <div className="mb-8">
+          <ToggleBar
+            title="Explicit &amp; Sensitive Content"
+            subTitle="Set this item as explicit and sensitive content"
+            icon={<Warning />}
+            checked={explicit}
+            onChange={() => setExplicit(!explicit)}
+          />
+        </div>
+
+        {/* Supply */}
+        <div className="mb-8">
+          <InputLabel
+            title="Supply"
+            subTitle="The number of items that can be minted."
+          />
+          <Input type="number" placeholder="1" disabled />
+        </div>
+
+        {/* Blockchain */}
+        <div className="mb-8">
+          <InputLabel title="Blockchain" />
+          <div className="relative">
+            <Listbox value={blockchain} onChange={setBlockChain}>
+              <Listbox.Button className="text-case-inherit letter-space-inherit flex h-10 w-full items-center justify-between rounded-lg border border-gray-200 bg-white px-4 text-sm font-medium text-gray-900 outline-none transition-shadow duration-200 hover:border-gray-900 hover:ring-1 hover:ring-gray-900 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100 dark:hover:border-gray-600 dark:hover:ring-gray-600 sm:h-12 sm:px-5">
+                <div className="flex items-center">
+                  <span className="ltr:mr-2 rtl:ml-2">{blockchain.icon}</span>
+                  {blockchain.name}
+                </div>
+                <ChevronDown />
+              </Listbox.Button>
+              <Transition
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Listbox.Options className="absolute left-0 z-10 mt-1 grid w-full origin-top-right gap-0.5 rounded-lg border border-gray-200 bg-white p-1 shadow-large outline-none dark:border-gray-700 dark:bg-gray-800 xs:p-2">
+                  {BlockchainOptions.map((option) => (
+                    <Listbox.Option key={option.id} value={option}>
+                      {({ selected }) => (
+                        <div
+                          className={`flex cursor-pointer items-center rounded-md px-3 py-2 text-sm text-gray-900 transition dark:text-gray-100  ${
+                            selected
+                              ? 'bg-gray-200/70 font-medium dark:bg-gray-600/60'
+                              : 'hover:bg-gray-100 dark:hover:bg-gray-700/70'
+                          }`}
+                        >
+                          <span className="ltr:mr-2 rtl:ml-2">
+                            {option.icon}
+                          </span>
+                          {option.name}
+                        </div>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </Listbox>
+          </div>
+        </div>
+
         <Button shape="rounded">CREATE</Button>
       </div>
     </>
