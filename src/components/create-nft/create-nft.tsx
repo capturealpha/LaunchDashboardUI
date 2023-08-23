7915cb15f1f890313af6156fec3973351b6d2f4c
KensieM <47368672+KensieM@users.noreply.github.com>
2023-08-23 15:20:48 -0600

    create page

--- src/components/create-nft/create-nft.tsx
+++ src/components/create-nft/create-nft.tsx
@@ -1,10 +1,5 @@
 import { useState } from 'react';
-import cn from 'classnames';
 import { NextSeo } from 'next-seo';
-import { Transition } from '@/components/ui/transition';
-import { Listbox } from '@/components/ui/listbox';
-import Image from '@/components/ui/image';
-import Button from '@/components/ui/button';
 import Input from '@/components/ui/forms/input';
 import Textarea from '@/components/ui/forms/textarea';
 import InputLabel from '@/components/ui/input-label';
@@ -47,71 +42,34 @@ export default function CreateNFT() {
         description="GPU-Enabled Jupyter Notebook Deployment Platform"
       />
       <div className="mx-auto w-full sm:pt-0 lg:px-8 xl:px-10 2xl:px-0">
-        <h2 className="mb-6 text-lg font-medium uppercase tracking-wider text-gray-900 dark:text-white sm:mb-10 sm:text-2xl">
-          Create New Deployment
-        </h2>
-        <div className="mb-8 grid grid-cols-1 gap-12 lg:grid-cols-3">
-          <div className="lg:col-span-2">
-            {/* File uploader */}
-            <div className="mb-8">
-              <InputLabel title="Upload file" important />
-              {/* Replace with your Uploader component */}
-            </div>
-            
-            {/* Price */}
-            <div className="mb-8">
-              <InputLabel title="Price Range" important />
-              <Input
-                min={0}
-                type="number"
-                placeholder="Enter your price range"
-                inputClassName="spin-button-hidden"
-              />
-            </div>
+        {/* ...Other parts of your code... */}
+        
+        {/* CPUs */}
+        <div className="mb-4">
+          <InputLabel title="CPUs" important />
+          <Input type="number" placeholder="Enter number of CPUs" onChange={(e) => setCPUs(e.target.value)} />
+        </div>
 
-            {/* Name */}
-            <div className="mb-8">
-              <InputLabel title="Name" important />
-              <Input type="text" placeholder="Item name" />
-            </div>
+        {/* RAM */}
+        <div className="mb-4">
+          <InputLabel title="RAM" important />
+          <Input type="number" placeholder="Enter RAM" onChange={(e) => setRAM(e.target.value)} />
+        </div>
 
-            {/* Description */}
-            <div className="mb-8">
-              <InputLabel
-                title="Description"
-                subTitle="The description will be included on the deployment's detail page."
-              />
-              <Textarea placeholder="Provide a detailed description of your deployment" />
-            </div>
+        {/* Storage */}
+        <div className="mb-4">
+          <InputLabel title="Storage" important />
+          <Input type="number" placeholder="Enter storage" onChange={(e) => setStorage(e.target.value)} />
+        </div>
 
-            {/* CPUs */}
-            <div className="mb-4">
-              <InputLabel title="CPUs" important />
-              <Input type="number" placeholder="Enter number of CPUs" onChange={(e) => setCPUs(e.target.value)} />
-            </div>
+        {/* Region */}
+        <div className="mb-4">
+          <InputLabel title="Region" important />
+          <Input type="text" placeholder="Enter region" onChange={(e) => setSelectedRegion(e.target.value)} />
+        </div>
 
-            <div className="mb-4">
-  <InputLabel title="RAM" important />
-  <div className="flex items-center space-x-2">
-    <Input type="number" placeholder="Enter RAM" onChange={(e) => setRAM(e.target.value)} />
-    <Dropdown options={['GB', 'MB']} selectedOption={selectedRAMUnit} onSelect={handleRAMUnitSelect} />
-  </div>
-</div>
-
-<div className="mb-4">
-  <InputLabel title="Storage" important />
-  <div className="flex items-center space-x-2">
-    <Input type="number" placeholder="Enter storage" onChange={(e) => setStorage(e.target.value)} />
-    <Dropdown options={['GB', 'TB']} selectedOption={selectedStorageUnit} onSelect={handleStorageUnitSelect} />
-  </div>
-</div>
-
-<div className="mb-4">
-  <InputLabel title="Region" important />
-  <Dropdown options={['Region A', 'Region B', 'Region C']} selectedOption={selectedRegion} onSelect={handleRegionSelect} />
-</div>
-
-
+        {/* ... Other fields ... */}
+        
         <Button shape="rounded">CREATE</Button>
       </div>
     </>
