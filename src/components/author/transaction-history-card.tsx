a15ad92f605523e7585453d4b23c9e66114230a6
KensieM <47368672+KensieM@users.noreply.github.com>
2023-08-24 13:20:26 -0600

    clean up

--- src/components/author/transaction-history-card.tsx
+++ src/components/author/transaction-history-card.tsx
@@ -18,11 +18,9 @@ type DeploymentHistoryCardProps = {
 export default function DeploymentHistoryCard({ item }: { item: DeploymentHistoryCardProps }) {
   const {
     author,
-    avatar,
     dateCreated,
     status,
     price,
-    gasFee,
   } = item ?? {};
   const bgColor = status === 'success' ? '#D2D786' : '#F2C672';
 
@@ -62,7 +60,22 @@ export default function DeploymentHistoryCard({ item }: { item: DeploymentHistor
             </div>
           </div>
           <div className="flex items-center lg:w-1/2">
+            <div className="flex flex-col truncate ltr:ml-2.5 rtl:mr-2.5 xl:ltr:ml-4 xl:rtl:mr-4">
+              <span className="mb-0.5 text-xs text-gray-400"></span>
+              <strong className="truncate font-medium -tracking-wider text-gray-900 dark:text-white">
+                {author} {/* Assuming the same author for now */}
+              </strong>
+            </div>
           </div>
+          <div className="flex flex-col truncate ltr:ml-2.5 rtl:mr-2.5 xl:ltr:ml-4 xl:rtl:mr-4">
+              <span className="mb-0.5 text-xs text-gray-400"></span>
+              <strong className="truncate font-medium -tracking-wider text-gray-900 dark:text-white">
+                {dateCreated} {/* Assuming the same author for now */}
+              </strong>
+            </div>
+        </div>
+        <div className="col-span-1 flex items-center text-gray-600 dark:text-gray-400 ltr:sm:pl-3 rtl:sm:pr-3 ltr:md:pl-0 rtl:md:pr-0 ltr:lg:pl-3 rtl:lg:pr-3">
+          <LongArrowRight className="h-5 w-5 rtl:rotate-180 md:h-6 md:w-6 lg:h-5 lg:w-5 xl:h-7 xl:w-7" />
         </div>
         <div className="col-span-4 flex flex-col gap-2.5 sm:flex-row sm:gap-x-4 md:flex-col 2xl:flex-row">
           <div className="flex items-center lg:w-1/2">
